561d19ef13232fef0ea520f02cece0ac3afdb963
ththune <135433132+ththune@users.noreply.github.com>
2024-04-01 11:33:52 +0200

    Create UserController.cs

---
+++ DotnetMinimalAPI/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetMinimalAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        [HttpGet("GetUsers/{testValue}")]
+        //public IActionResult Test()
+        public string[] GetUsers(string testValue)
+        {
+            string[] responseArray = new string[]
+            {
+                "test1",
+                "test2",
+                testValue,
+            };
+
+
+
+            return responseArray;
+        }
+    }
+}
